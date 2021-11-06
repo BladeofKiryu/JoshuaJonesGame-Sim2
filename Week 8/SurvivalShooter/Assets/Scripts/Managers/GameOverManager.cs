5eb47abdfc602864ebe8b1208863d2f4754e4117
BladeofKiryu <70967799+BladeofKiryu@users.noreply.github.com>
2021-11-05 22:17:29 -0500

    Week 8

---
+++ Week 8/SurvivalShooter/Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+    public Camera cam1;
+    public Camera cam2;
+
+
+    Animator anim;
+	float restartTimer;
+
+    void Start()
+    {
+        cam1.rect = new Rect(0, 0, 0.5f, 1);
+        cam2.rect = new Rect(0.5f, 0, 0.5f, 1);
+    }
+
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+			restartTimer += Time.deltaTime;
+
+			if (restartTimer >= restartDelay) {
+				Application.LoadLevel(Application.loadedLevel);
+			}
+        }
+    }
+}
